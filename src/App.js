6f919e5eb690ff530c76cbe16c5ea2e217415805
mgbiz <mgbizci@gmail.com>
2024-02-13 06:35:38 -0500

    Counter project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0)
+
+  const add = () =>{
+   setNumber(prevNumber => prevNumber + 1) 
+  }
+   const refresh = () => {
+     setNumber((prevNumber) => prevNumber = 0);
+   }
+    const substract = () => {
+      setNumber((prevNumber) => prevNumber -1);
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <p>Counter Project</p>
+        <h1>{number}</h1>
+        <div>
+          <button onClick={add}>+</button>
+          <button onClick={refresh}>refresh</button>
+          <button onClick={substract}>-</button>
+        </div>
+        <h3> &copy; IZCI development 2024</h3>
+      </section>
+    </main>
   );
 }
 
